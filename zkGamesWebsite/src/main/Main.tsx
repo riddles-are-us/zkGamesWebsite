5dc2bec61df388a230d359252663a2c5a5891ed3
charlie <charlie275691502@gmail.com>
2025-01-25 16:06:00 +1100

    AI Generating

---
+++ zkGamesWebsite/src/main/Main.tsx
@@ -0,0 +1,256 @@
+import React from "react";
+import "./style.css";
+
+export const Main = (): JSX.Element => {
+  return (
+    <div className="main">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <img
+              className="game"
+              alt="Game"
+              src="https://c.animaapp.com/m6UthH3E/img/game1.png"
+            />
+
+            <img
+              className="top-mask"
+              alt="Top mask"
+              src="https://c.animaapp.com/m6UthH3E/img/top-mask.png"
+            />
+
+            <div className="top-bar" />
+
+            <div className="search">
+              <div className="overlap-group-2">
+                <img
+                  className="img"
+                  alt="Search"
+                  src="https://c.animaapp.com/m6UthH3E/img/search-1@2x.png"
+                />
+
+                <input className="input" placeholder="Search" type="text" />
+              </div>
+            </div>
+
+            <img
+              className="alart"
+              alt="Alart"
+              src="https://c.animaapp.com/m6UthH3E/img/alart@2x.png"
+            />
+
+            <img
+              className="more"
+              alt="More"
+              src="https://c.animaapp.com/m6UthH3E/img/more@2x.png"
+            />
+
+            <div className="text-wrapper">Name name name</div>
+
+            <div className="select" />
+
+            <div className="text-wrapper-2">Automata</div>
+
+            <div className="text-wrapper-3">Play</div>
+
+            <p className="p">
+              Information Information Information Information Information
+            </p>
+
+            <div className="trending" />
+
+            <div className="text-wrapper-4">Trending</div>
+
+            <img
+              className="user"
+              alt="User"
+              src="https://c.animaapp.com/m6UthH3E/img/user@2x.png"
+            />
+          </div>
+
+          <div className="text-wrapper-5">Featured</div>
+
+          <div className="card">
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://c.animaapp.com/m6UthH3E/img/rectangle-1@2x.png"
+            />
+
+            <div className="overlap-2">
+              <div className="play">
+                <div className="frame">
+                  <div className="text-wrapper-6">Play</div>
+                </div>
+              </div>
+
+              <div className="frame-2">
+                <div className="text-wrapper-7">MemeDisco</div>
+
+                <div className="text-wrapper-8">
+                  information ajsfvijfpvpojsdcsdcv pdvjpowejc posvjvs
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-2">
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://c.animaapp.com/m6UthH3E/img/rectangle-1-1@2x.png"
+            />
+
+            <div className="overlap-2">
+              <div className="play">
+                <div className="frame">
+                  <div className="text-wrapper-6">Play</div>
+                </div>
+              </div>
+
+              <div className="frame-2">
+                <div className="text-wrapper-7">MemeDisco</div>
+
+                <div className="text-wrapper-8">
+                  information ajsfvijfpvpojsdcsdcv pdvjpowejc posvjvs
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-3">
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://c.animaapp.com/m6UthH3E/img/rectangle-1-2@2x.png"
+            />
+
+            <div className="overlap-2">
+              <div className="play">
+                <div className="frame">
+                  <div className="text-wrapper-6">Play</div>
+                </div>
+              </div>
+
+              <div className="frame-2">
+                <div className="text-wrapper-7">MemeDisco</div>
+
+                <div className="text-wrapper-8">
+                  information ajsfvijfpvpojsdcsdcv pdvjpowejc posvjvs
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-4">
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://c.animaapp.com/m6UthH3E/img/rectangle-1-3@2x.png"
+            />
+
+            <div className="overlap-2">
+              <div className="play">
+                <div className="frame">
+                  <div className="text-wrapper-6">Play</div>
+                </div>
+              </div>
+
+              <div className="frame-2">
+                <div className="text-wrapper-7">MemeDisco</div>
+
+                <div className="text-wrapper-8">
+                  information ajsfvijfpvpojsdcsdcv pdvjpowejc posvjvs
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="overlap-3">
+          <img
+            className="line"
+            alt="Line"
+            src="https://c.animaapp.com/m6UthH3E/img/line-1.svg"
+          />
+
+          <img
+            className="zkgame-logo"
+            alt="Zkgame logo"
+            src="https://c.animaapp.com/m6UthH3E/img/zkgame-logo-1@2x.png"
+          />
+
+          <div className="text-wrapper-9">More Information</div>
+
+          <p className="about-us-payment">
+            About us
+            <br />
+            Payment <br />
+            Privacy Policy <br />
+            Terms and Conditions
+          </p>
+
+          <img
+            className="line-2"
+            alt="Line"
+            src="https://c.animaapp.com/m6UthH3E/img/line-3.svg"
+          />
+
+          <img
+            className="line-3"
+            alt="Line"
+            src="https://c.animaapp.com/m6UthH3E/img/line-3.svg"
+          />
+
+          <img
+            className="info"
+            alt="Info"
+            src="https://c.animaapp.com/m6UthH3E/img/info-1@2x.png"
+          />
+
+          <div className="trending-wrapper">
+            <div className="trending-2">
+              <img
+                className="trending-icon"
+                alt="Trending icon"
+                src="https://c.animaapp.com/m6UthH3E/img/trending-icon@2x.png"
+              />
+
+              <div className="text-wrapper-10">Trending</div>
+            </div>
+          </div>
+
+          <div className="favourite">
+            <img
+              className="favourite-icon"
+              alt="Favourite icon"
+              src="https://c.animaapp.com/m6UthH3E/img/favourite-icon@2x.png"
+            />
+
+            <div className="text-wrapper-11">Favourite</div>
+          </div>
+
+          <div className="wallet">
+            <img
+              className="wallet-2"
+              alt="Wallet"
+              src="https://c.animaapp.com/m6UthH3E/img/wallet@2x.png"
+            />
+
+            <div className="text-wrapper-11">Wallet</div>
+          </div>
+
+          <div className="setting">
+            <img
+              className="setting-2"
+              alt="Setting"
+              src="https://c.animaapp.com/m6UthH3E/img/setting@2x.png"
+            />
+
+            <div className="text-wrapper-12">Setting</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
